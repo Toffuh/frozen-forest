4888f97ac5f776e73bf7fc8f9efced721221c755
to_binio <tobias.frischmann1@gmail.com>
2024-01-29 18:42:09 +0100

    improve camera movement prediction

--- src/camera.rs
+++ src/camera.rs
@@ -2,19 +2,15 @@ use crate::player::{Player, PlayerMovementSet};
 use bevy::app::App;
 use bevy::prelude::*;
 use bevy_xpbd_2d::components::LinearVelocity;
-use bevy_xpbd_2d::prelude::RigidBody;
+use bevy_xpbd_2d::prelude::{Position, PreviousPosition, RigidBody};
 use bevy_xpbd_2d::PhysicsSet;
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup).add_systems(
-            Update,
-            follow_player
-                .after(PlayerMovementSet::PlayerMovement)
-                .before(PhysicsSet::Prepare),
-        );
+        app.add_systems(Startup, setup)
+            .add_systems(Update, follow_player);
     }
 }
 
@@ -27,10 +23,11 @@ pub fn setup(mut commands: Commands) {
 }
 
 pub fn follow_player(
-    player_query: Query<(&Transform, &LinearVelocity), With<Player>>,
+    time: Res<Time>,
+    player_query: Query<(&Position, &PreviousPosition), With<Player>>,
     mut camera_query: Query<(&Transform, &mut LinearVelocity), (With<Camera>, Without<Player>)>,
 ) {
-    let Ok((player_transform, player_velocity)) = player_query.get_single() else {
+    let Ok((player_position, player_previous_position)) = player_query.get_single() else {
         return;
     };
 
@@ -38,7 +35,8 @@ pub fn follow_player(
         return;
     };
 
-    let predicted_position = player_transform.translation.xy() + player_velocity.0 * 1.2;
+    let predicted_position = player_position.0
+        + ((player_position.0 - player_previous_position.0) / time.delta_seconds()) * 20.;
 
     let change = predicted_position - camera_transform.translation.xy();
 
